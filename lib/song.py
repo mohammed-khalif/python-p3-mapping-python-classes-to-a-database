f3af580c10f7eff2ab2677d82407514c84d79de4
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-30 10:19:22 -0400

    complete

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,47 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+  
+#    class attribute
+#     name = "nimco"                        class attribute
+
+    def __init__(self, name, album):
+        #   //intances atrribute
+        self.id = None                          
+        self.name = name                    
+        self.album = album
+
+    # class method
+    # never take a self it take cls
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+
+        CURSOR.execute(sql)
+
+
+    def save(self):
+        sql = """
+        INSERT INTO songs (name, album)
+        VALUES (?,?)
+        """
+        
+        CURSOR.execute(sql,(self.name, self.album))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, album):    
+        song = Song(name, album)
+        song.save()
+        return song
+    
+
+
